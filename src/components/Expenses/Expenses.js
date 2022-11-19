ee64b9c7678ce2ed76589e489e9dad214b2599a4
Meghna Singh <meghnasingh1208@gmail.com>
2022-11-19 23:34:14 +0530

    [Meghna] Add. Displaying Items based on selected year from the dropdown

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,17 +6,26 @@ import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
+
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear)
         console.log(filteredYear);
     };
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
     return (
         <Card className="expenses">
-            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+            <ExpensesFilter
+                selected={filteredYear}
+                onChangeFilter={filterChangeHandler}
+            />
             {
-                props.items.map((expense) => (
+                filteredExpenses.map((expense) => (
                     <ExpenseItem
+                        key={expense.id}
                         title={expense.title}
                         amount={expense.amount}
                         date={expense.date}
