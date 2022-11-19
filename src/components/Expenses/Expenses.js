5e32390ec56c0276be090c580c897b8bc4520e24
Meghna Singh <meghnasingh1208@gmail.com>
2022-11-19 20:46:10 +0530

    [Meghna] Add. Filter by year dropdown

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,19 @@
+import React, { useState } from 'react';
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import './Expenses.css';
-import NewExpense from "../NewExpense/NewExpense";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear)
+        console.log(filteredYear);
+    };
 
     return (
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
