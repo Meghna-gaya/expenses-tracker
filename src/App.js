c957e823a218cbef3fb17cb350d8956d96611788
Meghna Singh <meghnasingh1208@gmail.com>
2022-11-19 16:01:12 +0530

    [Meghna] Refactor. Modularised code files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
     {
       id: 'e1',
-      title: 'Toilet Paper',
+      title: 'Notebook',
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e2', title: 'TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -17,7 +17,7 @@ function App() {
     },
     {
       id: 'e4',
-      title: 'New Desk (Wooden)',
+      title: 'Desk (Wooden)',
       amount: 450,
       date: new Date(2021, 5, 12),
     },
